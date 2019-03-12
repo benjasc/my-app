d549e4c80d623a09d0016d22ecca1560a292592f
Benjamin <benjamin.salazar17@gmail.com>
2019-03-12 18:08:14 -0400

    -

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:any [] = [];
+  constructor(private heroesService:HeroesService) { 
+  }
+
+  ngOnInit() {
+    this.heroes = this.heroesService.getHeroes();
+    console.log(this.heroes);
+    
+  }
+
+  verHeroe(id:Number){
+    
+    console.log(id);
+    
+  }
+
+}
